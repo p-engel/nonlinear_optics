d01235dd41323be9f7bd8a1a2f805cc801224edb
p-engel <anyanc>
2025-10-09 14:30:43 -0400

    unit test on dispersion index

--- optical_rectification/definitions.py
+++ optical_rectification/definitions.py
@@ -1,3 +1,4 @@
+import numpy as np
 """
 OR Simulation with consistent scaling
 -------------------------------------------
@@ -8,68 +9,67 @@ OR Simulation with consistent scaling
 """
 class Index():
 	"""
-    Refractive index n(ω) and absorption α(ω) 
-    with Lorentz model.
-    """
-    __init__(omega, omega0, gamma, osc_strength, n_inf, k):
+	Refractive index n(ω) and absorption α(ω) 
+	with Lorentz model.
+	"""
+	def __init__(omega, omega0, gamma, osc_strength, n_inf, k):
 		"""
 		Parameters
-	    ----------
-	    omega : np 1d array [rad/s]
-	    omega0 : np 1d array
-	    	resonant frequencies for n oscillators [rad/s]
-	    gamma : np 1d array
-	    	damping rates for n oscillators [rad/s]
-	    osc_strength : np 1d array
-	    	for n oscillators [(rad/s)^2]
-	    n_inf : float
-	    	real index value at infinity [1] 
-	    k : float
-	    	global absorption scaling factor [?]   
-    	"""
-    	self.w = omega; self.w0 = omega0; self.gamma = gamma
-    	self.a = osc_strength
-    	self.n_inf = n_inf; self.k = k
-    	self.n_osc = len(omega0)
-    	return
+		----------
+		omega : np 1d array [rad/s]
+		omega0 : np 1d array
+			resonant frequencies for n oscillators [rad/s]
+		gamma : np 1d array
+			damping rates for n oscillators [rad/s]
+		osc_strength : np 1d array
+			for n oscillators [(rad/s)^2]
+		n_inf : float
+			real index value at infinity [1] 
+		k : float
+			global absorption scaling factor [?]   
+		"""
+		self.w = omega; self.w0 = omega0; self.gamma = gamma
+		self.a = osc_strength
+		self.n_inf = n_inf; self.k = k
+		self.n_osc = len(omega0)
 
-    def lorentz(w0, gam0):
+	def lorentz(w0, gam0):
 		"""
 		Parameters
-	    ----------
-	    w0, gam0 : float [rad/s]
+		----------
+		w0, gam0 : float [rad/s]
 		Return
-	    ------
-	    f(ω) : np 1d array [1/(rad/s)^4]
-	    	un-normalized Lorentz PDF
-	    """
+		------
+		f(ω) : np 1d array [1/(rad/s)^4]
+			un-normalized Lorentz PDF
+		"""
 		f = 1 / (w0**2 - self.w**2)**2 + 4*(gam0**2)*(self.w**2)
 		return f
 
 	def n(self):
 		"""
 		Return
-	    ------
-	    n : np 1d array
-	    	real refractive index n(ω) [1]
-	    """
-	    n = np.full_like(self.w, self.n_inf, dtype=float)
-	    for i in self.n_osc:
-	    	real_part = self.a[i] * (self.w0[i]**2 - self.w**2)
-	    	n += real_part * lorentz(self.w0[i], self.gamma[i])
+		------
+		n : np 1d array
+			real refractive index n(ω) [1]
+		"""
+		n = np.full_like(self.w, self.n_inf, dtype=float)
+		for i in self.n_osc:
+			real_part = self.a[i] * (self.w0[i]**2 - self.w**2)
+			n += real_part * lorentz(self.w0[i], self.gamma[i])
 
-	    return n
+		return n
 
 	def alpha(self):
 		"""
 		Return
-	    ------
-	    alpha : np 1d array
-	    	imaginary refractive index α(ω) [?]
-	    """
-	    alpha = np.zeros_like(omega)
-	    for i in self.n_osc:
-	    	imag_part = self.k * self.a[i] * (2*self.gamma[i]*(self.w**2))
-	    	alpha += imag_part * lorentz(self.w0[i], self.gamma[i])
+		------
+		alpha : np 1d array
+			imaginary refractive index α(ω) [?]
+		"""
+		alpha = np.zeros_like(omega)
+		for i in self.n_osc:
+			imag_part = self.k * self.a[i] * (2*self.gamma[i]*(self.w**2))
+			alpha += imag_part * lorentz(self.w0[i], self.gamma[i])
 
-	    return alpha
+		return alpha
---
+++ unit_test/test_index.py
@@ -0,0 +1,1 @@
+from optical_rectification import definitions, par
