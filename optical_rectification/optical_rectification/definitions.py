fix mixing factor of squareroot of pi in fourier amplitude

optical_rectification/optical_rectification/definitions.py
@@ -224,21 +224,20 @@ class Gaussian():
         self.delta = 2 / self.tau  # 1 / e width in frequency domain
         self.w0 = w0
         self.E0 = E0
-        self.E0_w = E0 * self.tau
+        self.E0_w = E0 * np.sqrt(np.pi) * 2 / self.delta
         return
     
     def field_t(self, t):
         """ t - time 1d np array [s] """
-        E = self.E0 * np.exp( -1*np.array(t)**2 / self.tau**2 ) \
+        E = self.E0 * np.exp( -1 * (np.array(t) / self.tau)**2 ) \
             * np.exp( -1j*self.w0*np.array(t) )
         return E
 
     def field_w(self, w):
         """ w - frequency 1d np array [Hz] """
         E = self.E0_w \
-            * np.exp( 
-            	-1 * ( 2*np.pi*(self.w0 - np.array(w)) )**2 
-            	/ self.delta**2 
+            * np.exp( -1 *
+            	( 2*np.pi * (self.w0 - np.array(w)) / self.delta )**2 
             )
         return E
 

optical_rectification/unit_test/test_gaussian.py
@@ -20,9 +20,10 @@ def test_gaussian():
         I_peak = (EPS0*c/2) * np.abs(E_peak)**2
         I_w = (EPS0*c/2) * np.abs(E_w)**2
         
-        ind_fwhm = abs(1/2 - I_w/I_peak) < 1.2e-2
+        ind_fwhm = abs(1/2 - I_w/I_peak) < 1.2e-3
         w_fwhm = abs( max(w[ind_fwhm]) - min(w[ind_fwhm]) )
         print("The bandwith (FWHM) is ", w_fwhm, " [THz]." )
+        print( pulse.delta * np.sqrt(2 * np.log(2)) / (2*np.pi) )
         cond = abs((t_fwhm * w_fwhm) - TBP) < 1e-2
         assert cond, "The pulse doesn't satisfy the time-bandwith product"
 