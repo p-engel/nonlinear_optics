311c3a3bde4922d6441118b3963170841f8d82df
p-engel <anyanc>
2026-02-04 20:22:04 -0500

    implement three photon absorption

--- optical_rectification/optical_rectification/propagator.py
+++ optical_rectification/optical_rectification/propagator.py
@@ -6,31 +6,27 @@ Dispersion, Index
 
 class ORPropagator:
     def __init__(self, Ω_max, pulse, cascade=True):
-        self.pulse = pulse
-        self.w = pulse.w
-        self.dw = self.w[1] - self.w[0]
+        self.Nw = len(pulse.w)
+        self.dw = pulse.w[1] - pulse.w[0]
         self.Ω = arange(1, int(Ω_max/self.dw) + 1) * self.dw
 
+        self.index_w = Index(pulse.w, param=par.param_op, k=1)
         self.index_Ω = Index(self.Ω, param=par.param_thz)
-        self.index_w = Index(self.w, param=par.param_op, k=1)
-
+        
         self.alpha_w = self.index_w.alpha()
         self.alpha_Ω = self.index_Ω.alpha()
 
         self.dispersion = Dispersion(
-            self.w, self.index_w.sellmeier(), 
+            pulse.w, self.index_w.sellmeier(), 
             Ω=self.Ω, n_Ω=self.index_Ω.n()
         )
 
-        self.pref_w = chi2_factor(self.w, self.dispersion.k)
+        self.pref_w = chi2_factor(pulse.w, self.dispersion.k)
         self.pref_Ω = chi2_factor(self.Ω, self.dispersion.k_Ω)
-
-        self.Nw = len(self.w)
-        self.NΩ = len(self.Ω)
 
         self.cascade = cascade
 
-        self.Ew0 = self.pulse.field_w()
+        self.Ew0 = pulse.field_w()
         self.EΩ0 = zeros_like(self.Ω, dtype=complex)
 
     def pack(self, Ew, EΩ):
